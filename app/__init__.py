d7c71bb5467248896d27e64573c761b9523d4ad5
Jay Kang <promotion.kang.jay@gmail.com>
2023-01-24 00:12:23 -0600

    cleanup

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from config import Config
-from .receipts.receipts import receipts
+from .receipts.receipts import receipts  # registering a blueprint
 
 app = Flask(__name__)
 app.register_blueprint(receipts)
--- config.py
+++ config.py
@@ -4,5 +4,5 @@ basedir = os.path.abspath(os.path.dirname(__name__))
 
 class Config:
     FLASK_APP = os.environ.get('FLASK_APP')
-    FLASK_ENV = os.environ.get('FLASK_ENV')
+    FLASK_DEBUG = os.environ.get('FLASK_DEBUG')
     SECRET_KEY = '1234'
